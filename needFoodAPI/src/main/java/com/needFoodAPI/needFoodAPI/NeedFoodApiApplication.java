97e51b8729223aab04191b90b1bf53f58cb043d1
Thomas Ferrer <thomas.ferrer@hei.yncrea.fr>
2021-11-27 19:14:51 +0100

    init spring api

---
+++ needFoodAPI/src/main/java/com/needFoodAPI/needFoodAPI/NeedFoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.needFoodAPI.needFoodAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeedFoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeedFoodApiApplication.class, args);
+	}
+
+}
---
+++ needFoodAPI/src/test/java/com/needFoodAPI/needFoodAPI/NeedFoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.needFoodAPI.needFoodAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeedFoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
